a7165e92b2f17b9a18c1640529dc0ada3715f1f1
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-03-06 21:24:44 +0100

    creando videolist

--- src/main.jsx
+++ src/main.jsx
@@ -2,21 +2,31 @@ import ReactDom from 'react-dom'
 import VideoItem from './components/VideoItem'
 import './index.css'
 
+const VideoList = ({ title, children }) => {
+	return (
+		<div>
+			<h1>{title}</h1>
+			{children}
+		</div>
+	)
+}
+
 const app = (
 	<>
-		<h1>Vamos que podemos.</h1>
-		<VideoItem
-			title='Componentes'
-			duracion={2760}
-			uploadDate={new Date(2022, 2, 25)}
-			description='Componentes en React'
-		></VideoItem>
-		<VideoItem
-			title='useState'
-			duracion={3145}
-			uploadDate={new Date(2022, 3, 26)}
-			description='Cómo utilizar estados en React'
-		></VideoItem>
+		<VideoList title='Curso de React'>
+			<VideoItem
+				title='Componentes'
+				duracion={2760}
+				uploadDate={new Date(2022, 2, 25)}
+				description='Componentes en React'
+			></VideoItem>
+			<VideoItem
+				title='useState'
+				duracion={3145}
+				uploadDate={new Date(2022, 3, 26)}
+				description='Cómo utilizar estados en React'
+			></VideoItem>
+		</VideoList>
 	</>
 )
 
